f43ff46b0a44ebf9617c008a9055c6b74b22701d
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-06-22 12:34:55 +0530

    Rating entity added

--- src/main/java/com/frontline/ratingservice/model/Rating.java
+++ src/main/java/com/frontline/ratingservice/model/Rating.java
@@ -1,4 +1,38 @@
 package com.frontline.ratingservice.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Rating {
+    @Id
+    @GeneratedValue
+    private Integer ratingId;
+    private String productId;
+    private Integer rating;
+
+    public Integer getRatingId() {
+        return ratingId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRatingId(Integer ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
 }
